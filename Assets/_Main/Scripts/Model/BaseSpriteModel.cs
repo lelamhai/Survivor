494684af9bf5e859ec1e9282db299b563a751ed6
lelamhai <lelamhai@ADMIN>
2023-06-10 15:13:14 +0700

    Flip model

--- Assets/_Main/Scripts/Model/BaseSpriteModel.cs
+++ Assets/_Main/Scripts/Model/BaseSpriteModel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public abstract class BaseSpriteModel : BaseMonoBehaviour
@@ -7,19 +8,35 @@ public abstract class BaseSpriteModel : BaseMonoBehaviour
     [SerializeField] private SpriteRenderer _model = null;
     [SerializeField] private BaseMove _baseMove = null;
     [SerializeField] private Vector2 _currentPos = Vector2.zero;
-
+    [SerializeField] private NetworkVariable<Vector2> _networkPlayerPosition= new NetworkVariable<Vector2>();
     private void LateUpdate()
     {
+        if (IsClient && IsOwner)
+        {
+            InputClient();
+        }
+
         SetFlip();
+    }
+
+    private void InputClient()
+    {
+        UpdateClientPositionServerRpc(_baseMove.GetCurrentPos());
     }
 
     private void SetFlip()
     {
-        _currentPos = _baseMove.GetCurrentPos();
-        if (_currentPos.x != 0)
+        //_currentPos = _baseMove.GetCurrentPos();
+        if (_networkPlayerPosition.Value.x != 0)
         {
-            _model.flipX = _currentPos.x < 0;
+            _model.flipX = _networkPlayerPosition.Value.x < 0;
         }
+    }
+
+    [ServerRpc]
+    public void UpdateClientPositionServerRpc(Vector2 pos)
+    {
+        _networkPlayerPosition.Value = pos;
     }
 
     protected override void SetDefaultValue()
